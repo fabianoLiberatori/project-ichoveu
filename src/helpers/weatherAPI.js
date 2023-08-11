cd418b84f5c07edd8e60201eeb7c204589ef5b6a
Fabiano Liberatori <fabiano29585@gmail.com>
2023-08-11 09:45:12 -0300

    feat: searchCities implementada

--- src/helpers/weatherAPI.js
+++ src/helpers/weatherAPI.js
@@ -1,7 +1,17 @@
-// Remova os comentários a medida que for implementando as funções
+const token = import.meta.env.VITE_TOKEN;
 
-export const searchCities = (/* term */) => {
-//   seu código aqui
+export const searchCities = async (term) => {
+  const searchUrl = `http://api.weatherapi.com/v1/search.json?lang=pt&key=${token}&q=${term}`;
+  try {
+    const fetchCitie = await fetch(searchUrl);
+    const data = await fetchCitie.json();
+    if (data.length === 0) {
+      window.alert('Nenhuma cidade encontrada');
+    }
+    return data;
+  } catch (error) {
+    throw error.messege;
+  }
 };
 
 export const getWeatherByCity = (/* cityURL */) => {
